87612b5e281b77350ff8679731d784907b3a21df
yayabingbong <97674510+sauftot@users.noreply.github.com>
2022-12-28 12:46:46 +0100

    merged changes from private repo

--- scripts/ShowCaseSoT.py
+++ scripts/ShowCaseSoT.py
@@ -4,43 +4,44 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException, NoSuchWindowException
+from selenium.common.exceptions import TimeoutException, NoSuchWindowException, NoSuchElementException
 
 """
     OBS-Python Script by vp to post sea of thieves reaper faction wins on twitch channel.
     Requires everything above and a valid nightbot api token in chromedriver.exe's directory.
 """
 
-# THE EMPTY STRINGS NEED TO BE FILLED OUT BY YOU, YOU CAN GET THIS INFO FROM YOUR NIGHTBOT PANNEL, APPLICATION SETTINGS
 data = lambda: ...
-data.streaming = False
-data.setup = False
+
+# THE EMPTY STRINGS NEED TO BE FILLED OUT BY YOU USING YOUR NIGHTBOT API INFORMATION AND COMMAND ID
 data.client_ID = ''
 data.client_secret = ''
-# RUN THE INCLUDED SCRIPT getNightBotCommands.py AND FIND THE ID IF YOUR CUSTOM !WINS AND !LEVEL COMMAND, THEN FILL THIS
 data.winsID = ''
 data.levelID = ''
-# THIS WILL BE GENERATED DONT FILL!
+# 0 is Reaper, 1 is Athena PvP Faction, set as you want
+data.faction = 0
+
+# INTERNAL DATA, DO NOT FILL !!!
+data.run = False
+data.setup = False
+data.setupRunning = False
 data.token = ''
 
-
-
-    
+  
 def worker_thread():
     sleep(2)
     while not data.setup:
-        sleep(2) 
-        if not data.streaming:
-            print("INFO: Streaming was cancelled before setup was complete. You can close all windows")
-            return 
-    if not data.streaming:
-        print("INFO: Streaming was cancelled before setup was complete. You can close all windows")
-        return
+        print("ERROR: Stats Synchronization was toggled on before setup was complete! Complete setup to start sync'ing.")
+        if not data.run:
+            print("INFO: Cancelling Sync...")
+            return
+        sleep(3)
     
-    print("INFO: Worker reached.")
+    print("INFO: Worker reached and set up.")
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    driver = webdriver.Chrome(executable_path='../chromedriver.exe', chrome_options=options)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(chrome_options=options)
     wins = 0
     levels = 0
     try:
@@ -52,13 +53,16 @@ def worker_thread():
     sleep(4)
     load_cookie(driver)
     
-    while data.streaming:
-        driver.get("https://www.seaofthieves.com/de/profile/reputation/FactionB")
+    while data.run:
+        if data.faction == 0:
+            driver.get("https://www.seaofthieves.com/de/profile/reputation/Flameheart")
+        else:
+            driver.get("https://www.seaofthieves.com/de/profile/reputation/PirateLord")
         sleep(5)
         try:
             element = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div/div[3]/div/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div[3]")
         except NoSuchElementException:
-            print("ERROR: Finding Element. Cookies most likely invalid. Reload Script.")
+            print("ERROR: Can't find element. Cookies invalid or website layout changed. Reload Script.")
             driver.close()
             return
         if element.text != wins:
@@ -72,13 +76,15 @@ def worker_thread():
             print("INFO: Wins Updated")
             s.close()
             wins = element.text
-        # push to nightbot _id:638f6b04cb154ffa1a28aa4e
         driver.get("https://www.seaofthieves.com/de/profile/reputation/")
         sleep(5)
         try:
-            element = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div/div[3]/div/div[2]/button[12]/div/div/div/div[2]/div/div/div/div/div")
+            if data.faction == 0:
+                element = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div/div[3]/div/div[2]/button[12]/div/div/div/div[2]/div/div/div/div/div")
+            else:
+                element = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div/div[3]/div/div[2]/button[11]/div/div/div/div[2]/div/div/div/div/div")
         except NoSuchElementException:
-            print("ERROR: Finding Element. Cookies most likely invalid. Reload Script.")
+            print("ERROR: Can't find element. Cookies invalid or website layout changed. Reload Script.")
             driver.close()
             return
         if element.text != levels:
@@ -92,15 +98,12 @@ def worker_thread():
             print("INFO: Level Updated")
             s.close()
             levels = element.text
-        # push to nightbot _id:638f8ece1d5766902edb13ec
-        
-    print("INFO: Stopped streaming.")
+    print("INFO: Stopped sync'ing.")
     driver.close()
     return
-        
-        
+      
 def setup():
-    driver = webdriver.Chrome(executable_path='../chromedriver.exe')
+    driver = webdriver.Chrome()
     sleep(2)
     try:
         try:
@@ -108,12 +111,14 @@ def setup():
         except TimeoutException:
             print("ERROR: Timeout received. Internet Connection stable?")
             data.setup = False
+            data.setupRunning = False
             driver.close()
             return
         try:
             WebDriverWait(driver, timeout=120).until(lambda d: d.get_cookie('rat'))['value']
         except TimeoutException:
             data.setup = False
+            data.setupRunning = False
             driver.close()
             print("ERROR: SoT Login Timeout!")
             return
@@ -125,47 +130,77 @@ def setup():
         if data.token == '':
             print("ERROR: NightBot Token void.")
             data.setup = False
+            data.setupRunning = False
             return
         print("INFO: NightBot Token received.")
         data.setup = True
+        data.setupRunning = False
         return
     except NoSuchWindowException:
         print("ERROR: The Window was unexpectedly closed.")
         data.setup = False
+        data.setupRunning = False
         return
     
+    
+### OBS Functions
 
 def script_load(settings):
-    starterThread = threading.Thread(target=setup)
-    starterThread.start()
-    OBS.obs_frontend_add_event_callback(on_streaming)
     print("INFO: Script Initialized")
-    if OBS.obs_frontend_recording_active():
-        t = threading.Thread(target=worker_thread)
-        data.streaming = True
-        t.start()
         
 def script_unload():
-    data.streaming = False
+    data.run = False
     dire = os.path.expandvars(r'%APPDATA%')
     if data.setup:
         os.remove(dire + '/DO_NOT_CHANGE.txt')
     revokeToken(data.token)
     print("INFO: Script unloaded.")
-        
+    
+def script_properties():
+    props = OBS.obs_properties_create()
+    setupButton = OBS.obs_properties_add_button(props, 'Setup', 'Setup', cb_setupButton)
+    OBS.obs_property_set_visible(setupButton, True)
+    toggleButton = OBS.obs_properties_add_button(props, 'Sync', 'Sync', cb_toggleButton)
+    OBS.obs_property_set_visible(toggleButton, True)
+    factionButton = OBS.obs_properties_add_button(props, 'Toggle Faction', 'Faction: Reaper', cb_factionButton)
+    return props
+    
+    
+def script_description():
+    return "Make sure to update the DATA values inside the script with the information retrieved from executing the getNightBotCommands.py script!"
+  
+    
+## Callbacks
 
+def cb_setupButton(props, prop, *args, **kwargs):
+    if not data.setupRunning:
+        if data.setup:
+            data.setup = False
+        data.setupRunning = True
+        starterThread = threading.Thread(target=setup)
+        starterThread.start()
+        print("INFO: Starting Setup Thread...")
+    else:
+        print("INFO: Setup is already running, please reload script.")
 
-def on_streaming(event):
-    t = threading.Thread(target=worker_thread)
-    if event == OBS.OBS_FRONTEND_EVENT_RECORDING_STARTED:
-        if data.setup == True:
-            data.streaming = True
-            t.start()
-        else:
-            print("ERROR: Setup is not completed!, please reload the script.")
-    if event == OBS.OBS_FRONTEND_EVENT_RECORDING_STOPPED:
-        data.streaming = False
-
+def cb_toggleButton(props, prop, *args, **kwargs):
+    if data.run == False:
+        t = threading.Thread(target=worker_thread)
+        data.run = True
+        t.start()
+        return
+    data.run = False
+    
+def cb_factionButton(props, prop, *args, **kwargs):
+    p = OBS.obs_properties_get(props, "Toggle Faction")
+    if data.faction == 0:
+        OBS.obs_property_set_description(p, "Faction: Athena")
+        data.faction = 1
+    else:
+        OBS.obs_property_set_description(p, "Faction: Reaper")
+        data.faction = 0
+    print("INFO: Faction = " + str(data.faction))
+    return True
 
 
 #### Helper Functions ####
